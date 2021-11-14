1e4224c2056ef6caa578b20a10faa0c368e2e7ab
Diana Udisteanu <diana.udisteanu@yahoo.com>
2021-11-14 20:04:18 +0200

    Add stack navigator to make connection between screens.

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from '../screens/login';
+
+
+const Stack = createStackNavigator();
+function AddIntoStack(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login"
+                              component={Login}
+                              options={({navigation}) => ({
+                                headerShown: false,
+                                gestureEnabled: false
+                            })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AddIntoStack;
