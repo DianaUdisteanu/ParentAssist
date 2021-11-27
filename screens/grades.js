e19c198f3b1e0aed6f262ffb26f7e402dca50620
Denis <avramdenis58@gmail.com>
2021-11-27 18:16:22 +0200

    Almost finished the design for parents screens.

---
+++ screens/grades.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, Image, SafeAreaView} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import GradesObject from '../components/grades-object';
+
+const DataGrades = [
+    {
+        key:1,
+        name:"MATHEMATICS",
+        gradeOne:"10 / 29.11.2021",
+        gradeTwo:"8 / 6.12..2021",
+        gradeThree:"4 / 5.5.2021"
+        
+    },
+    {
+        key:2,
+        name:"HISTORY",
+        gradeOne:"10 / 29.11.2021",
+        gradeTwo:"8 / 6.12..2021",
+        gradeThree:"4 / 5.5.2021"
+    },
+    {
+        key:3,
+        name:"GEOGRAPHY",
+        gradeOne:"10 / 29.11.2021",
+        gradeTwo:"",
+        gradeThree:"4 / 5.5.2021"
+    },{
+        key:4,
+        name:"ART",
+        gradeOne:"10 / 29.11.2021",
+        gradeTwo:"8 / 6.12..2021",
+        gradeThree:""
+    },
+    {
+        key:5,
+        name:"BIOLOGY",
+        gradeOne:"10 / 29.11.2021",
+        gradeTwo:"8 / 6.12..2021",
+        gradeThree:""
+    },
+    {
+        key:6,
+        name:"CHEMESTRY",
+        gradeOne:"",
+        gradeTwo:"",
+        gradeThree:""
+    }
+]
+
+export default class Grades extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+        };
+    }
+
+    render(){
+        return(
+            <View style={{ flex: 1 }}>
+                <View style={{flex: 0.10, flexDirection: "row", justifyContent: "space-between"}}>
+                <TouchableOpacity onPress={() => this.props.navigation.reset({index:0, routes:[{name:'Login'}]})} style={{width: 30, height: 30, marginTop:"12%", marginLeft: "3%"}}>
+                        <Image source={require("../app/images/logout_blue.png")} style={{width: 30, height: 30}} resizeMode='contain'/>
+                </TouchableOpacity>
+                < Text style={{color: "#2d3a56", fontSize:19, fontFamily:'bold-font', fontWeight:'bold', marginTop: "13%"}}>
+                       GRADES
+                </Text>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate("EditProfile")} style={{width: 30, height: 30, marginTop:"12%", marginRight: "3%", marginTop:"12%"}}>
+                        <Image source={require("../app/images/settings_icon_blue.png")} style={{width: 30, height: 30}} resizeMode='contain'/>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flex: 0.9}}>
+                <Text style={{color: "#96A793", fontSize:19, fontFamily:'bold-font', fontWeight:'bold', marginTop: "17%"}}>
+                       GRADES
+                </Text>
+                <View style={{backgroundColor:"#96A793",width:"90%", borderRadius:30, flexDirection: 'row', alignContent:'center',marginHorizontal:"5%"}}>
+                    <View style={{flexDirection: 'column', alignItems:'center', justifyContent:'space-between'}}>
+                        <GradesObject name={DataGrades[0].name} gradeOne={DataGrades[0].gradeOne} gradeTwo={DataGrades[0].gradeTwo} gradeThree={DataGrades[0].gradeThree} />
+                        <GradesObject name={DataGrades[1].name} gradeOne={DataGrades[1].gradeOne} gradeTwo={DataGrades[1].gradeTwo} gradeThree={DataGrades[1].gradeThree} />
+                        <GradesObject name={DataGrades[2].name} gradeOne={DataGrades[2].gradeOne} gradeTwo={DataGrades[2].gradeTwo} gradeThree={DataGrades[2].gradeThree} />
+                    </View>
+                    <View style={{flexDirection: 'column', alignItems:'center', justifyContent:'space-between'}}>
+                        <GradesObject name={DataGrades[3].name} gradeOne={DataGrades[3].gradeOne} gradeTwo={DataGrades[3].gradeTwo} gradeThree={DataGrades[3].gradeThree} />
+                        <GradesObject name={DataGrades[4].name} gradeOne={DataGrades[4].gradeOne} gradeTwo={DataGrades[4].gradeTwo} gradeThree={DataGrades[4].gradeThree} />
+                        <GradesObject name={DataGrades[5].name} gradeOne={DataGrades[5].gradeOne} gradeTwo={DataGrades[5].gradeTwo} gradeThree={DataGrades[5].gradeThree} />
+                    </View>
+                </View>
+            </View>
+            </View>
+        );
+    }
+}
---
+++ screens/posts.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, Image, SafeAreaView} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import AnnouncementPosted from '../components/announcement_posted';
+
+const DataPosts = [
+    {
+        key:1,
+        title:"TITLE ONE",
+        imageSRC:"../app/images/adev.png",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap  into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        key:2,
+        title:"TITLE TWO",
+        imageSRC:"../app/images/adev.png",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        key:3,
+        title:"TITLE THREE",
+        imageSRC:"",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    },{
+        key:4,
+        title:"TITLE FOUR",
+        imageSRC:"../app/images/adev.png",
+        description: ""
+    }
+]
+
+
+export default class Posts extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            userType: "parent"
+        };
+    }
+
+    render(){
+        return(
+        <View style={{ flex: 1 }}>
+            <View style={{flex: 0.10, flexDirection: "row", justifyContent: "space-between"}}>
+                <TouchableOpacity onPress={() => this.props.navigation.reset({index:0, routes:[{name:'Login'}]})} style={{width: 30, height: 30, marginTop:"12%", marginLeft: "3%"}}>
+                        <Image source={require("../app/images/logout_blue.png")} style={{width: 30, height: 30}} resizeMode='contain'/>
+                </TouchableOpacity>
+                < Text style={{color: "#2d3a56", fontSize:19, fontFamily:'bold-font', fontWeight:'bold', marginTop: "13%"}}>
+                        {this.state.userType == 'parent' ? "TEACHER POSTS" : this.state.userType == 'teacher' ? "PARENTS POSTS" : null}
+                </Text>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate("EditProfile")} style={{width: 30, height: 30, marginTop:"12%", marginRight: "3%", marginTop:"12%"}}>
+                        <Image source={require("../app/images/settings_icon_blue.png")} style={{width: 30, height: 30}} resizeMode='contain'/>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flex: 0.9}}>
+                    <SafeAreaView style={{}}>
+                        <FlatList   data={ DataPosts }
+                                    renderItem={ ({item}) => {return <AnnouncementPosted title={item.title} hasImage={item.hasImage} imageSRC={item.imageSRC} hasDescription={item.hasDescription} description={item.description}/>}}
+                                    keyExtractor={ (student) => student.key }
+                        />
+                    </SafeAreaView>
+                </View>
+        </View>
+        );
+    }
+}
