5314e14b9ff83294318c5ebc5041e87792b6687a
Denis <avramdenis58@gmail.com>
2022-01-06 23:08:27 +0200

    Finish implementing the creation of parent replies.

--- screens/confirm_image.js
+++ screens/confirm_image.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 import { IconButton } from 'react-native-paper';
-
+import TouchHistoryMath from 'react-native/Libraries/Interaction/TouchHistoryMath';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getDatabase, ref, onValue} from "firebase/database";
 export default class ConfirmImage extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            email: "",
+            role: ""
+        };
     }
+
+    async componentDidMount() {
+       await this.handleGetEmail();
+       this.handleGetRole();
+    }
+
+
+    handleGetEmail = async() =>{
+        try{
+            const value = await AsyncStorage.getItem("email");
+            if(value !== null) {
+                this.setState({email : value});
+            }
+        }catch(e){}
+    }
+
+    handleGetRole = () => {
+        const db = getDatabase();
+        const starCountRef = ref(db, '/users/' + this.state.email + '/Role');
+        onValue(starCountRef, (snapshot)=> {
+            this.setState({role: snapshot.val()});
+        })
+    }
+
 
     render() {
         return(                     
@@ -30,15 +59,21 @@ export default class ConfirmImage extends React.Component {
                                         size={30}
                                         color="white"
                                         style={{backgroundColor:"#262731"}}
-                                        onPress={() =>  
-                                                        this.props.navigation.navigate("SecondScreen",
-                                                        {
-                                                         screen:'Announcement',
-                                                         params: {
-                                                             imageURI: this.props.route.params.image.uri
-                                                         }   
-                                                        }
-                                                    )
+                                        onPress={() =>  {
+                                                        this.state.role === "Parent" ?
+                                                            this.props.navigation.navigate("ThirdScreen",
+                                                            {
+                                                            screen:'ParentReply',
+                                                            params: {
+                                                                imageURI: this.props.route.params.image.uri
+                                                            }   
+                                                            }) : this.state.role === "Teacher" ?  this.props.navigation.navigate("SecondScreen",
+                                                            {
+                                                            screen:'Announcement',
+                                                            params: {
+                                                                imageURI: this.props.route.params.image.uri
+                                                            }   
+                                                            }) : null}
                                         }
                             />
                 </View>
--- screens/parent_posts.js
+++ screens/parent_posts.js
@@ -2,45 +2,70 @@ import React from 'react';
 import {View, TouchableOpacity, Text, Image, SafeAreaView} from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import AnnouncementPosted from '../components/announcement_posted';
-
-const DataPosts = [
-    {
-        key:1,
-        title:"TITLE ONE",
-        imageSRC:"../app/images/adev.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap  into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-    },
-    {
-        key:2,
-        title:"TITLE TWO",
-        imageSRC:"../app/images/adev.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-    },
-    {
-        key:3,
-        title:"TITLE THREE",
-        imageSRC:"",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-    },{
-        key:4,
-        title:"TITLE FOUR",
-        imageSRC:"../app/images/adev.png",
-        description: ""
-    }
-]
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getDatabase, ref, onValue} from "firebase/database";
 
 
 export default class ParentPosts extends React.Component{
     constructor(){
         super();
-        this.state = {};
+        this.state = {
+            email: "",
+            DataPosts :[]
+        };
+    }
+
+    async componentDidMount() {
+        await this.handleGetEmail();
+        this.handleGetPosts();
+    }
+
+    handleGetEmail = async() =>{
+        try{
+            const value = await AsyncStorage.getItem("email");
+            if(value !== null) {
+                this.setState({email : value});
+            }
+        }catch(e){}
+    }
+
+    handleGetPosts = () => {
+        let count = 0;
+        const db = getDatabase();
+        const absPath = "/users/" + this.props.route.params.parentAccount + '/announcements';
+        const studentPath = ref(db, absPath);
+        onValue(studentPath, (snapshot) => {
+            onValue(studentPath, (snapshot) => {
+            let tempArray = []
+                snapshot.forEach( (childSnapshot2) => {
+                    let countPost = 1;
+                    count = count + 1;
+                    const postPath = absPath + "/" + childSnapshot2.key;
+                    const postPathDB = ref(db, postPath);
+                    onValue( postPathDB, (snapshot) => {
+                        countPost = 1;
+                        let objPost = {}
+                        snapshot.forEach( (childSnapshot) => {
+                            if (countPost === 1) objPost.description = childSnapshot.val();
+                            else if (countPost === 2) objPost.imageSRC = childSnapshot.val();
+                            else if (countPost === 3) objPost.title = childSnapshot.val();
+                            else objPost.type = childSnapshot.val();
+                            countPost = countPost + 1;
+                        });
+                        objPost.key = count;
+                        tempArray.push(objPost);
+                    });
+                });
+                this.setState({DataPosts:tempArray});
+            });
+        });
     }
 
     render(){
         return(
         <View style={{ flex: 1 }}>
             <View style={{flex: 0.10, flexDirection: "row", justifyContent: "space-between"}}>
-                <TouchableOpacity onPress={() => this.props.navigation.reset({index:0, routes:[{name:'Login'}]})} style={{width: 30, height: 30, marginTop:"12%", marginLeft: "3%"}}>
+                <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{width: 30, height: 30, marginTop:"12%", marginLeft: "3%"}}>
                         <Image source={require("../app/images/back_arrow_blue.png")} style={{width: 30, height: 30}} resizeMode='contain'/>
                 </TouchableOpacity>
                 < Text style={{color: "#2d3a56", fontSize:19, fontFamily:'bold-font', fontWeight:'bold', marginTop: "13%"}}>
@@ -48,9 +73,12 @@ export default class ParentPosts extends React.Component{
                 </Text>
                 <View style={{flex:0.2}}></View>
             </View>
+            <View style={{flex:0.05, flexDirection:'column', alignItems:'center'}}>
+                    <Text style={{color: "#2d3a56", fontSize:15, fontFamily:'bold-font', fontWeight:'bold'}}>{this.props.route.params.parentFullName.toUpperCase()}</Text>
+            </View>
             <View style={{ flex: 0.9}}>
                     <SafeAreaView style={{}}>
-                        <FlatList   data={ DataPosts }
+                        <FlatList   data={ this.state.DataPosts }
                                     renderItem={ ({item}) => {return <AnnouncementPosted title={item.title} hasImage={item.hasImage} imageSRC={item.imageSRC} hasDescription={item.hasDescription} description={item.description}/>}}
                                     keyExtractor={ (student) => student.key }
                         />
