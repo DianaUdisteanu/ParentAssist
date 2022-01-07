3791c7751e3f15e68cf07e785e72af0c47163a54
Diana Udisteanu <diana.udisteanu@yahoo.com>
2022-01-07 20:35:59 +0200

    Open galery for uploading a profile picture.

--- screens/edit_profile.js
+++ screens/edit_profile.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {View, TouchableOpacity, Text, Image, Pressable} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref as ref_database, onValue, set} from "firebase/database";
+import * as ImagePicker from 'expo-image-picker';
+import { getStorage, ref as ref_storage, uploadBytesResumable, getDownloadURL  } from "firebase/storage";
 
 export default class EditProfile extends React.Component{
     constructor(){
@@ -10,9 +12,59 @@ export default class EditProfile extends React.Component{
             fullEmail: "",
             email: "",
             name: "",
-            phone: ""
+            phone: "",
+            imageURI: null,
+            imageSRC: null
         };
     }
+
+    pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        if (!result.cancelled) {
+          this.setState({imageURI: result.uri});
+        }
+
+        let blob = await fetch(
+            this.state.imageURI
+        ).then(
+            response => response.blob()
+        )
+
+        let metadata = {
+            type:'image/jpeg'
+        }
+
+        let file = new File([blob],'test.jpeg',metadata)
+
+        const storage = getStorage();
+        const imagesReference = ref_storage(storage, 'image/' + new Date().toISOString());
+        const uploadImage = uploadBytesResumable(imagesReference, file, metadata)
+
+        uploadImage.on( 'state_changed',
+                        (snapshot) => {},
+                        (error)=>{console.log(error.message)},
+                        ()=>{getDownloadURL(uploadImage.snapshot.ref).then(async(downloadURL)=>{console.log(downloadURL)
+                            const db = getDatabase();
+                            const userReference = ref_database(db, 'users/'+ this.state.email)
+                            onValue(userReference, (snapshot) => {
+                                const data = snapshot.val();
+                                let pictureURL = {
+                                    imageURL: downloadURL
+                                }
+                                const pictureReference = ref_database(db,'users/'+this.state.email+'/'+'picture')
+                                set( pictureReference, pictureURL)
+                                }
+                            )
+                        })
+        })
+
+    };
 
     async componentDidMount(){
         await this.handleGetFullEmail();
@@ -40,13 +92,17 @@ export default class EditProfile extends React.Component{
 
     GetDetails = () =>{
         const db = getDatabase();
-        const starCountRef = ref(db, '/users/' + this.state.email + '/Name');
+        const starCountRef = ref_database(db, '/users/' + this.state.email + '/Name');
         onValue(starCountRef, (snapshot)=> {
             this.setState({name: snapshot.val()});
         })
-        const starCountRef2 = ref(db, '/users/' + this.state.email + '/Phone');
+        const starCountRef2 = ref_database(db, '/users/' + this.state.email + '/Phone');
         onValue(starCountRef2, (snapshot)=> {
             this.setState({phone: snapshot.val()});
+        })
+        const starCountRef3 = ref_database(db, '/users/' + this.state.email + '/picture' + '/imageURL');
+        onValue(starCountRef3, (snapshot)=> {
+            this.setState({imageSRC: snapshot.val()});
         })
     }
 
@@ -60,7 +116,9 @@ export default class EditProfile extends React.Component{
                     < Text style={{color: "#2d3a56", fontSize:19, fontFamily:'bold-font', fontWeight:'bold', marginTop: "13%", flex: 0.65, textAlign:'left'}}>EDIT PROFILE</Text>
                 </View>
                 <View style={{flex: 0.25, justifyContent: "center", alignItems: 'center'}}>
-                    <Image source={require("../app/images/profile_picture_placeholder_blue.png")} style={{width: "75%", height: "75%"}} resizeMode='contain'/>
+                    <Image
+                        source={{uri:this.state.imageSRC}}
+                        style={{width: "75%", height: "75%", borderRadius:1000}} resizeMode='contain'/>
                 </View>
                 <View style={{flex: 0.30,flexDirection: "column", justifyContent: "space-around", alignItems: "center",  }}>
                     <View>
@@ -90,7 +148,7 @@ export default class EditProfile extends React.Component{
                 </View>
                 <View style={{ flex: 0.25, marginBottom:"5%", justifyContent :"space-around", marginTop: "5%"}}>
                         <Pressable style={{backgroundColor: '#96A793', alignItems:'center', width:"50%", marginHorizontal:"25%", height:47, justifyContent:'center', borderRadius:30, marginTop: "2%"}}
-                                onPress={()=> console.log("set profile picture")} > 
+                                onPress={this.pickImage} > 
                             <Text  style={{color:'white', fontFamily:'bold-font', fontSize:17}}>SET PROFILE PICTURE</Text>
                         </Pressable>
                         <Pressable style={{backgroundColor: '#96A793', alignItems:'center', width:"50%", marginHorizontal:"25%", height:47, justifyContent:'center', borderRadius:30, marginBottom: "7%"}}
